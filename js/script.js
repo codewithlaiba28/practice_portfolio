0c8e810d343e91af9a144aa31e2099bf67bad0be
Laiba Khan <codewithlaiba28@gmail.com>
2025-09-29 01:12:23 +0500

    add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 /* === toggle icon navbar === */
-let menuIcon = document.querySelectorAll('#menu_icon')
+let menuIcon = document.querySelector('#menu_icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active')
+}
 
 /* === scroll sections avtive link === */
 let sections = document.querySelectorAll('section');
@@ -22,6 +28,34 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    /* === remove toggle icon and navbar when clicknavbar link(scroll) === */
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active')
 };
 
 
+/* === scroll reveal=== */
+ScrollReveal({ 
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200 
+});
+
+ScrollReveal().reveal('.home_content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home_img, .services_container, .portfolio_box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home_content h1, .about_img', { origin: 'left'});
+ScrollReveal().reveal('.home_content p, .about_content', { origin: 'right'});
+
+
+
+
+/* === typed js=== */
+const typed = new Typed('.multiple-text', {
+    strings: ['Python', 'Html', 'CSS', 'Tailwind', 'Javascript', 'Agentic AI', 'Generative AI', 'n8n'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
